ca130bd719740ac93ba7806c3a562f8831453607
vitszeng <vitszeng@outlook.com>
2024-08-07 10:10:13 +0800

    add git etc.

--- newsoft4llinux.sh
+++ newsoft4llinux.sh
@@ -5,4 +5,10 @@ sudo apt install axel wget curl aria2 -y
 
 sudo apt install htop zsh fcitx5 fcitx5-pinyin fcitx5-rime fcitx5-chinese-addons -y
 
+sudo apt install tmux screen neofetch -y
+
+sudo apt install git gcc vim openssh-server -y
+
+sudo apt install filezilla -y
+
 sudo apt install -f
