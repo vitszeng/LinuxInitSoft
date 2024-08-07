9898cb005f6e793417fe65ce9f16bbe7d5087007
vitszeng <vitszeng@outlook.com>
2024-08-07 09:51:40 +0800

    First commit

---
+++ newsoft4llinux.sh
@@ -0,0 +1,8 @@
+
+sudo apt install proxychains shadowsocks-libev v2ray -y
+
+sudo apt install axel wget curl aria2 -y
+
+sudo apt install htop zsh fcitx5 fcitx5-pinyin fcitx5-rime fcitx5-chinese-addons -y
+
+sudo apt install -f
